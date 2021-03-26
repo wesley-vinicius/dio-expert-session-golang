8f43b52b233b86da96ce5a24d7854ed8a916e1be
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-03-26 19:40:50 -0300

    prometheus integration

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/wesley-vinicius/planning_finance/adapter/http/actuator"
 	"github.com/wesley-vinicius/planning_finance/adapter/http/transaction"
 	"net/http"
@@ -11,6 +12,8 @@ func Init() {
 	http.HandleFunc("/transactions/create", transaction.CreateTransactions)
 
 	http.HandleFunc("/health", actuator.Health)
+
+	http.Handle("/metrics", promhttp.Handler())
 
 	http.ListenAndServe(":8080", nil)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wesley-vinicius/planning_finance
 go 1.13
 
 require (
+	github.com/prometheus/client_golang v1.10.0
 	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
 	golang.org/x/tools v0.1.0 // indirect
 )
